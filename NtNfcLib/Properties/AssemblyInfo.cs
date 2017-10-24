6093c72560b03fd21e88f47043d0c9b48613f6ff
kazyx <dev.ykz@gmail.com>
2017-10-25 03:09:11 +0900

    Update project files for Visual Studio 2017 v15

--- NtNfcLib/Properties/AssemblyInfo.cs
+++ NtNfcLib/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,14 +13,6 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("536e35b7-f3ff-4aba-8c78-86e39e6c0b36")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -34,4 +24,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: ComVisible(false)]
